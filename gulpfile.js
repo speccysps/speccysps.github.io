ca5f62c5f92b838f065279b78a5554cd9c97ac09
speccysps <shaun.skeen@icloud.com>
2019-12-12 18:02:34 +0000

    Gulpfile scss compile into css

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
-var gulp = require('gulp')
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
+
+// ! compile scss into css
+function style() {
+    // 1. where is my scss file
+    return gulp.src('./_scss/**/*.scss')
+    // 2. pass that file through sass compiler
+    .pipe(sass())
+    // 3. where do I save the complied css?
+    .pipe(gulp.dest('./assets/css'))
+}
+
+exports.style = style;
