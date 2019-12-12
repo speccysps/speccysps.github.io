5edacc5dc7ffbd9b08d23f7fe107fc4a4692a555
speccysps <shaun.skeen@icloud.com>
2019-12-12 18:27:00 +0000

    Gulpfile browserSync working correctly
    SCSS Compiles into css file and browser reloads

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,29 @@ const gulp = require('gulp'),
 
 // ! compile scss into css
 function style() {
-    // 1. where is my scss file
+    // ? 1. where is my scss file
     return gulp.src('./_scss/**/*.scss')
-    // 2. pass that file through sass compiler
+    // ? 2. pass that file through sass compiler
     .pipe(sass())
-    // 3. where do I save the complied css?
+    // ? 3. where do I save the complied css?
     .pipe(gulp.dest('./assets/css'))
+    // ? 4. stream changes to all browser (browserSync) 
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    // ? watch the css folder and perform the 'style' function defined above
+    gulp.watch('./_scss/**/*.scss', style);
+    // ? watch the html file and reload the page when changes detected
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    // ? watch the js folder and reload the page when changes detected
+    gulp.watch('./_js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
