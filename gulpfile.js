efd997dc4fdcd9e358bc4f85fd34cb6ee32be52f
speccysps <shaun.skeen@icloud.com>
2019-12-15 14:29:09 +0000

    Move jQuery and Bootstrap files into src

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,32 @@ const gulp = require('gulp'),
 function style() {
     // ? 1. where is my scss file
     return gulp.src('./_scss/**/*.scss')
-    // ? 2. pass that file through sass compiler
-    .pipe(sass().on('error', sass.logError))
-    // ? 3. where do I save the complied css?
-    .pipe(gulp.dest('./src/css'))
-    // ? 4. stream changes to all browser (browserSync) 
-    .pipe(browserSync.stream());
+        // ? 2. pass that file through sass compiler
+        .pipe(sass().on('error', sass.logError))
+        // ? 3. where do I save the complied css?
+        .pipe(gulp.dest('./src/css'))
+        // ? 4. stream changes to all browser (browserSync) 
+        .pipe(browserSync.stream());
 }
+
+// ! Move jquery into src folder
+function jquery() {
+    // ? 1. where is my jquery.min.js file
+    return gulp.src('./node_modules/jquery/dist/jquery.min.js')
+        // ? 2. where do I move the jquery file
+        .pipe(gulp.dest('./src/js'))
+
+}
+
+// ! Move bootstrap into src folder
+function bootstrap() {
+    // ? 1. where is my bootstrap.min.js file
+    return gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
+        // ? 2. where do I move the jquery file
+        .pipe(gulp.dest('./src/js'))
+
+}
+
 
 // ! watch files & folders
 function watch() {
@@ -30,6 +49,8 @@ function watch() {
 }
 
 exports.style = style;
+exports.jquery = jquery;
+exports.bootstrap = bootstrap;
 exports.watch = watch;
 
 // * Default
