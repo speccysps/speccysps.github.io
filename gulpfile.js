6b25ec525ecf9cf729946a09595eef801d1eff65
speccysps <shaun.skeen@icloud.com>
2019-12-17 12:23:31 +0000

    Gulp now builds Jekyll and serves through BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    shell = require('gulp-shell');
 
 // ! compile scss into css
 function style() {
@@ -30,26 +31,34 @@ function bootstrap() {
         .pipe(gulp.dest('./src/js'))
 }
 
+// ! Jekyll task
+function jekyllBuild(done) {
+shell.task(['bundle exec jekyll serve'])(done);
+}
+
 // ! watch files & folders
 function watch() {
     browserSync.init({
         server: {
-            baseDir: './'
+            baseDir: '_site/'
         }
     });
     // ? watch the css folder and perform the 'style' function defined above
     gulp.watch('./_scss/**/*.scss', style);
     // ? watch the html file and reload the page when changes detected
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
     // ? watch the js folder and reload the page when changes detected
     gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+    // ? watch the markdown files and reload the page when changes detected
+    gulp.watch('./**/*.markdown').on('change', browserSync.reload);
 }
 
 exports.style = style;
 exports.jquery = jquery;
 exports.bootstrap = bootstrap;
+exports.jekyllBuild = jekyllBuild;
 exports.watch = watch;
 
 // * Default
-const build = gulp.series(jquery, bootstrap, watch);
+const build = gulp.series(jquery, bootstrap, gulp.parallel(jekyllBuild, watch));
 exports.default = build;
